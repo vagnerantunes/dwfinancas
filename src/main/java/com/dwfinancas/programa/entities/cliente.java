fc8e16933e1b1bf8d97566f05dfa243749753bde
vagnerantunes <vagner.antunes0701@outlook.com>
2021-04-23 00:06:07 -0300

    alteração em entidade cliente

--- src/main/java/com/dwfinancas/programa/entities/cliente.java
+++ src/main/java/com/dwfinancas/programa/entities/cliente.java
@@ -1,53 +1,58 @@
 package com.dwfinancas.programa.entities;
 
 import java.io.Serializable;
+import java.time.Instant;
 import java.time.LocalDate;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "tb_cliente")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class cliente implements Serializable{
 
-	private static final long serialVersionUID = 1L;
-	
-	
-	
-	public cliente(String cLI_NOME, String cLI_TIPO, String cLI_CFP_CNPJ, LocalDate cLI_DATA_NASCIMENTO,
-			String cLI_CONTATO, String cLI_EMAIL, String cLI_RAZAOSOCIAL, String cLI_NOME_FANTASIA, String cLI_ENDERECO,
-			String cLI_SEGMENTO) {		
-		CLI_NOME = cLI_NOME;
-		CLI_TIPO = cLI_TIPO;
-		CLI_CFP_CNPJ = cLI_CFP_CNPJ;
-		CLI_DATA_NASCIMENTO = cLI_DATA_NASCIMENTO;
-		CLI_CONTATO = cLI_CONTATO;
-		CLI_EMAIL = cLI_EMAIL;
-		CLI_RAZAOSOCIAL = cLI_RAZAOSOCIAL;
-		CLI_NOME_FANTASIA = cLI_NOME_FANTASIA;
-		CLI_ENDERECO = cLI_ENDERECO;
-		CLI_SEGMENTO = cLI_SEGMENTO;
-	}
+	private static final long serialVersionUID = 1L;				
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long CLI_ID;
+	
+	@Column(length = 45, nullable = false)
 	private String CLI_NOME;
+	@Column(length = 15, nullable = false)
 	private String CLI_TIPO;
-	private String CLI_CFP_CNPJ;
-	private LocalDate CLI_DATA_NASCIMENTO;
+	@Column(length = 15)
+	private String CLI_CFPCNPJ;
+	
+	private Instant CLI_DATANASCIMENTO;
+	
+	@Column(length = 15)
 	private String CLI_CONTATO;
+	
+	@Column(length = 45)
 	private String CLI_EMAIL;
+	
+	@Column(length = 45)
 	private String CLI_RAZAOSOCIAL;
-	private String CLI_NOME_FANTASIA;
+	
+	@Column(length = 45)
+	private String CLI_NOMEFANTASIA;
+	
+	@Column(length = 45)
 	private String CLI_ENDERECO;
+	
+	@Column(length = 45)
 	private String CLI_SEGMENTO;
 
 }
