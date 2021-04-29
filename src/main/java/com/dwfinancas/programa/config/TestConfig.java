4628321046e508845620ca29bac1069bd464bf37
vagnerantunes <vagner.antunes0701@outlook.com>
2021-04-29 09:06:20 -0300

    inserção de registro na classe testConfig de forma de pagamento

--- src/main/java/com/dwfinancas/programa/config/TestConfig.java
+++ src/main/java/com/dwfinancas/programa/config/TestConfig.java
@@ -9,8 +9,10 @@ import org.springframework.context.annotation.Profile;
 
 import com.dwfinancas.programa.entities.Cliente;
 import com.dwfinancas.programa.entities.Fatura;
+import com.dwfinancas.programa.entities.FormaPagamento;
 import com.dwfinancas.programa.repositories.ClienteRepository;
 import com.dwfinancas.programa.repositories.FaturaRepository;
+import com.dwfinancas.programa.repositories.FormaPagamentoRepository;
 
 @Configuration
 @Profile("test")
@@ -19,15 +21,24 @@ public class TestConfig implements CommandLineRunner {
 	@Autowired
 	private ClienteRepository clienteRepository;
 	
+	@Autowired
 	private FaturaRepository faturaRepository;
+	
+	@Autowired
+	private FormaPagamentoRepository formaPagamentoRepository; 
 
 	@Override
 	public void run(String... args) throws Exception {
-		Fatura fatura = new Fatura(null, null, null, null, null, null, null, null, null, null, null, null);
-		faturaRepository.saveAll(Arrays.asList(fatura));	
 		
-		Cliente cliente = new Cliente(null, null, null, null, null, null, null, null, null, null, null, null);
-		clienteRepository.saveAll(Arrays.asList(cliente));
+		FormaPagamento fpg1 = new FormaPagamento(null, null, null, null, null, null, null, null);
+		formaPagamentoRepository.saveAll(Arrays.asList(fpg1));
+		
+		Fatura fat1 = new Fatura(null, null, null, null, null, null, null, null, null, null, null, null);		
+		faturaRepository.saveAll(Arrays.asList(fat1));
+		
+		//Fatura(Long, FormaPagamento, FaturaStatus, String, LocalDate, String, String, BigDecimal, BigDecimal, BigDecimal, LocalDate, Integer)
+		Cliente c1 = new Cliente(null, null, null, null, null, null, null, null, null, null, null, null);
+		clienteRepository.saveAll(Arrays.asList(c1));
 		
 		
 	}
