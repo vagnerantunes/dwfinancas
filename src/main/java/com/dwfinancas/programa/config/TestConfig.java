2e3ae44623daace7080e889708d9592544b7195a
vagnerantunes <vagner.antunes0701@outlook.com>
2021-05-14 13:50:09 -0300

    povoando classes usuario, parcelaVenda

--- src/main/java/com/dwfinancas/programa/config/TestConfig.java
+++ src/main/java/com/dwfinancas/programa/config/TestConfig.java
@@ -11,10 +11,14 @@ import org.springframework.context.annotation.Profile;
 import com.dwfinancas.programa.entities.Cliente;
 import com.dwfinancas.programa.entities.Fatura;
 import com.dwfinancas.programa.entities.FormaPagamento;
+import com.dwfinancas.programa.entities.ParcelaVenda;
+import com.dwfinancas.programa.entities.Usuario;
 import com.dwfinancas.programa.enums.PagamentoStatus;
 import com.dwfinancas.programa.repositories.ClienteRepository;
 import com.dwfinancas.programa.repositories.FaturaRepository;
 import com.dwfinancas.programa.repositories.FormaPagamentoRepository;
+import com.dwfinancas.programa.repositories.ParcelaVendaRepository;
+import com.dwfinancas.programa.repositories.UsuarioRepository;
 
 @Configuration
 @Profile("test")
@@ -27,12 +31,20 @@ public class TestConfig implements CommandLineRunner {
 	private FaturaRepository faturaRepository;
 	
 	@Autowired
-	private FormaPagamentoRepository formaPagamentoRepository; 		
+	private FormaPagamentoRepository formaPagamentoRepository;
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@Autowired
+	private ParcelaVendaRepository parcelaVendaRepository; 
 	
 	@Override
 	public void run(String... args) throws Exception {
 		
-		Cliente cli1 = new Cliente(null, "Vagner Antunes de Souza", "PF", "097.551.319-22", Instant.parse("1994-01-07T10:00:00Z"), "(43) 99139-7159", "vagner.antunes0701@outlook.com", null, null, "RUA SOFIA KUSKOSK MENDES, N°134", null, null, null);
+		Cliente cli1 = new Cliente(null, "Vagner Antunes de Souza", "PF", "097.551.319-22", 
+				Instant.parse("1994-01-07T10:00:00Z"), "(43) 99139-7159", "vagner.antunes0701@outlook.com", null, null, 
+				"RUA SOFIA KUSKOSK MENDES, N°134", null, null, null);
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		
 		//Cliente(Long, String, String, String, Instant, String, String, String, String, String, String, List<Venda>)
@@ -40,9 +52,15 @@ public class TestConfig implements CommandLineRunner {
 		FormaPagamento fpg1 = new FormaPagamento(null, "PAGAMENTO", "DAS", "MENSAL", 1, 0.00, null, null, null);
 		formaPagamentoRepository.saveAll(Arrays.asList(fpg1));
 				
-		Fatura fat2 = new Fatura(null, fpg1, Instant.parse("2021-05-06T08:15:00Z"), 61.00, 0.00, 0.00, 0.00, Instant.parse("2021-06-06T00:00:00Z"), null, PagamentoStatus.PENDENTE);
+		Fatura fat2 = new Fatura(null, fpg1, Instant.parse("2021-05-06T08:15:00Z"), 61.00, 0.00, 0.00, 0.00, 
+				Instant.parse("2021-06-06T00:00:00Z"), null, PagamentoStatus.PENDENTE);		
 		faturaRepository.saveAll(Arrays.asList(fat2));
-		//Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
-		//Fatura(Long, FormaPagamento, PagamentoStatus, String, Instant, String, String, Double, Double, Double, Instant, Integer)								
+		
+		Usuario usu1 = new Usuario(null, "Vagner Antunes", "A", "Proprietario", "123456", null);
+		usuarioRepository.saveAll(Arrays.asList(usu1));
+		
+		ParcelaVenda pvd1 = new ParcelaVenda(null, 1, 300.00, 0.00, Instant.parse("2021-05-14T00:00:00Z"), 1, PagamentoStatus.PENDENTE, null);
+		parcelaVendaRepository.saveAll(Arrays.asList(pvd1));
+		
 	}		
 }
