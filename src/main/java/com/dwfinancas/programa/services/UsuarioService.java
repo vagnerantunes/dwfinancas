10620fdab5e2549986e7a0dbc0ff3ccbe18863ad
vagnerantunes <vagner.antunes0701@outlook.com>
2021-06-09 12:49:32 -0300

    atualização de usuário

--- src/main/java/com/dwfinancas/programa/services/UsuarioService.java
+++ src/main/java/com/dwfinancas/programa/services/UsuarioService.java
@@ -15,22 +15,39 @@ public class UsuarioService {
 	@Autowired
 	private UsuarioRepository repository;
 	
+	//listar todos usuarios
 	public List<Usuario> findAll(){
 		return repository.findAll(); 
 	}
 	
+	//buscar usuario por codigo
 	public Usuario findById(Long id) {
 		Optional<Usuario> obj = repository.findById(id);
 		return obj.get();		
 	}
 	
+	//inserir usuario
 	public Usuario insert(Usuario obj) {
 		return repository.save(obj);
 		
 	}
 	
+	//deletar usuario
 	public void delete(Long id) {
 		repository.deleteById(id);
+	}
+	
+	//atualizar usuario
+	public Usuario update(Long id, Usuario obj) {
+		Usuario entity = repository.getOne(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(Usuario entity, Usuario obj) {
+		entity.setUSU_NOME(obj.getUSU_NOME());
+		entity.setUSU_FLAG(obj.getUSU_FLAG());
+		entity.setUSU_FUNCAO(obj.getUSU_FUNCAO());
 	}
 	
 }
