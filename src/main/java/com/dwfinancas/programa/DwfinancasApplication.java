5aa112f0ee3711ca7faeaaca5ef222ed46e37b1c
vagnerantunes <vagner.antunes0701@outlook.com>
2023-05-15 23:29:34 -0300

    Exclusao da dependencia s3 da AWS

--- src/main/java/com/dwfinancas/programa/DwfinancasApplication.java
+++ src/main/java/com/dwfinancas/programa/DwfinancasApplication.java
@@ -1,26 +1,12 @@
 package com.dwfinancas.programa;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.dwfinancas.programa.services.S3Service;
-
 @SpringBootApplication
-public class DwfinancasApplication implements CommandLineRunner {
-	
-	@Autowired
-	private S3Service s3Service;
-	
-	public static void main(String[] args) {
-		SpringApplication.run(DwfinancasApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		s3Service.uploadFile("C:\\Users\\Vagner Antunes\\Desktop\\imagens");
-		
-	}
+public class DwfinancasApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DwfinancasApplication.class, args);
+    }
 
 }
